b89cc34c1ab602889df3b1f422663b9ca1d05436
C Λ M I N Λ S H Ξ L L <92443358+caminashell@users.noreply.github.com>
2024-01-18 15:27:36 +0000

    Added source file + doc to project

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # iplsch
+
 IP Address List Checker
+
+I haven't written any Python in a while so...
+
+I was installing UFW on Debian and looking into IP Blocking lists.
+Since I already use a well-maintained list, I thought I would compare
+it to what I had come across from a UFW source.
+
+For now, you'll need two files for the script to process;
+
+1. `base.txt` - The file that contains your current IP Block list.
+2. `test.txt` - The file containing another list of IP addresses to block.
+
+It will print out a list of all the IP Addresses that are missing from your
+control (base) file.
+
+Example:
+
+```log
+Missing: 192.168.1.0
+Missing: 192.168.1.1
+Missing: 192.168.1.2
+Missing: 192.168.1.3
+```
+
+Run this script alongside your list files with;
+
+```bash
+python iplsch.py
+```
+
+-- or --
+
+```sh
+python3 iplsch.py
+```
---
+++ iplsch.py
@@ -0,0 +1,32 @@
+# Import regular expressions.
+import re
+
+# Create an empty array object (LIST).
+ipList = []
+
+# File to use as baseline. e.g. List from Marius.
+with open('base.txt', "r") as file1:
+    # Simple for each line in file
+    for line in file1:
+        # Sanitise line/string and push it to array.
+        ipList.append(line.strip())
+
+# File to use query against baseline. e.g. List from UFW.
+# https://github.com/poddmo/ufw-blocklist
+with open('test.txt', "r") as file2:
+    # Simple for each line in file
+    for line in file2:
+        # Assign variable to regex filter of line/string (IP).
+        ipAddr = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line )[0]
+        # Evaluate if the string (IP) exists within the array (LIST).
+        if ipAddr not in ipList:
+            # If the above condition is true,
+            # split the string (IP) off to a variable.
+            d = line.split()
+            # Finally, print out the result on the screen.
+            print('Missing:', d[0])
+
+# Future considerations;
+            # 1. Memory clean up.
+            # 1. Operators and Inputs.
+            # 1. Compiling to a binary.
